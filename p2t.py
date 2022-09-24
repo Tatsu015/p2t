129f10d99cb09ad1be2663718b07ab22b21a318d
Tatsu015 <tatsumi.soichiro@gmail.com>
2022-09-24 16:12:22 +0900

    add usecase class

---
+++ p2t.py
@@ -0,0 +1,33 @@
+from select import select
+from snipper import Snipper
+from translator import Translator
+import pyperclip
+import threading
+
+class P2T:
+    def __init__(self, snipper:Snipper, translator:Translator) -> None:
+        self.snipper = snipper
+        self.translator = translator
+
+    def __del__(self):
+        self.stop()
+
+    def start(self)->None:
+        thread = threading.Thread(target=self.__run)
+        thread.start()
+
+    def stop(self)->None:
+        pyperclip.copy('')
+        
+    def __run(self)->None:
+        try:
+            self.snipper.clear()
+            img = self.snipper.wait_for_snip()
+        except:
+            print('exit waiting')
+            return
+
+        text = self.translator.translate(img)
+        print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+        print(text)
+        print('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
